ffc3671aedd5005ba8caf05640cdfb29c94018ba
Pradeep-Gopal <pradeepprachu@gmail.com>
2020-09-29 13:08:42 -0400

    Changed object name in main

--- app/main.cpp
+++ app/main.cpp
@@ -15,12 +15,12 @@
 
 int main() {
   // Object created by calling parameterized constructor
-  PIDController control(0.05, 0.4, 0.0001, 0.005);
+  PIDController pid(0.05, 0.4, 0.0001, 0.005);
   float target_velocity = 400.0;
   float actual_velocity = 20.0;
   std::cout << "The actual velocity is = " << actual_velocity << std::endl;
   std::cout << "The target velocity is = " << target_velocity << std::endl;
   std::cout << "The New velocity using PID controller is = "
-            << control.compute(target_velocity, actual_velocity) << std::endl;
+            << pid.compute(target_velocity, actual_velocity) << std::endl;
   return 0;
 }
